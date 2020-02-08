d6869e4323aa78fef3a99cfce853fddf08cae6dc
Daniel Urbina <danielur1999@gmail.com>
2020-02-08 00:41:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,9 +10,7 @@ end
 
 def find_max_value(array)
 x = 0 
-array.length.times { |index|
-  if array[index] > x
-    x = array[index]
+array.length.times { |index| if array[index] > x x = array[index]
   end
 }
 end
